aefdcf43d856194ef8fd6e0da0f5d718169ce509
Jung0 <awf.15.92@gmail.com>
2021-04-10 16:38:18 +0900

    Update Gradle 7.0RC2 -> 7.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    google()
-    gradlePluginPortal()
-    mavenCentral()
-}
-
 dependencies {
     implementation(libs.android.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,6 +1,19 @@
-// https://docs.gradle.org/7.0-rc-2/release-notes.html
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+// https://docs.gradle.org/7.0/release-notes.html
 enableFeaturePreview("VERSION_CATALOGS")
+
 dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
